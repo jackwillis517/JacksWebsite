12c97b1724851d1800101ea5a70dab32ba1d33fb
jackwillis517 <jackwillis517@gmail.com>
2021-09-24 16:04:26 -0400

    First website commit

---
+++ main.js
@@ -0,0 +1,32 @@
+
+const menuBtn = document.querySelector(".menu-btn");
+const mobileMenu = document.querySelector(".mobileMenu");
+const helloWorldContainer = document.querySelector(".helloWorld-subcontainer");
+const aboutContainer = document.querySelector(".about-container");
+
+let menuOpen = false;
+menuBtn.addEventListener("click", () => {
+  if (!menuOpen) {
+    menuBtn.classList.add("open");
+    mobileMenu.classList.add("display-nav");
+    helloWorldContainer.classList.add("hide");
+    menuOpen = true;
+  } else {
+    menuBtn.classList.remove("open");
+    mobileMenu.classList.remove("display-nav");
+    helloWorldContainer.classList.remove("hide");
+    menuOpen = false;
+  }
+});
+
+
+const tabs = document.querySelectorAll(".navmenu ul li a");
+tabs.forEach((item) => {
+  item.addEventListener("click", () => {
+    for (var i = 0; i < tabs.length; i++) {
+      tabs[i].classList.remove("active");
+    }
+    this.classList.add("active");
+  });
+});
+
