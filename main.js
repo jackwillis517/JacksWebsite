1b705cc58dac37f86b83bb1296feba277bf01734
jackwillis517 <jackwillis517@gmail.com>
2021-12-22 10:44:18 -0500

    Added contact form and portfolio page that needs content

--- main.js
+++ main.js
@@ -17,6 +17,30 @@ function removeActiveClasses() {
   });
 }
 
+// const myForm = document.getElementById("my-form");
+    
+// async function handleSubmit(event) {
+//   event.preventDefault();
+//   var status = document.getElementById("status");
+//   var data = new FormData(event.target);
+//   fetch(event.target.action, {
+//     method: form.method,
+//     body: data,
+//     headers: {
+//         'Accept': 'application/json'
+//     }
+//   }).then(response => {
+//     status.innerHTML = "Thanks for your submission!";
+//     status.classList.add('success')
+//     form.reset()
+//   }).catch(error => {
+//     status.innerHTML = "Oops! There was a problem submitting your form"
+//     status.classList.add('error')
+//   });
+// }
+// myForm.addEventListener("submit", handleSubmit)
+
+
 let menuOpen = false;
 menuBtn.addEventListener("click", () => {
   if (!menuOpen) {
